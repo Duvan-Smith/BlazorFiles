112dabce7dc98d4249ae5d2338b645ce715c847b
Duvan Smith Montenegro Roque <dsmith.mr@gmail.com>
2021-05-18 08:58:05 -0500

    Agregar archivos de proyecto.

---
+++ BlazorFiles.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Blazor.FileReader;
+using MatBlazor;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Threading.Tasks;
+
+namespace BlazorFiles.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddMatBlazor();
+            builder.Services.AddFileReaderService(options =>
+            {
+                options.UseWasmSharedBuffer = true;
+            });
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
