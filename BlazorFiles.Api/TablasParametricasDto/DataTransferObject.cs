de039ac61f6f93fde76dd84e0134d6af14c4858a
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-05-18 16:33:19 -0500

    clean code

--- BlazorFiles.Api/TablasParametricasDto/DataTransferObject.cs
+++ BlazorFiles.Api/TablasParametricasDto/DataTransferObject.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BlazorFiles.Api.TablasParametricasDto
+﻿namespace BlazorFiles.Api.TablasParametricasDto
 {
     public class DataTransferObject
     {
