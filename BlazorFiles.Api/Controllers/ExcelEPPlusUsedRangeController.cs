8dd168b68797f1a9d3e6c9f008de72d0ca32fc3d
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-05-19 14:40:00 -0500

    Commit the changes in develop for Epplus

--- BlazorFiles.Api/Controllers/ExcelEPPlusUsedRangeController.cs
+++ BlazorFiles.Api/Controllers/ExcelEPPlusUsedRangeController.cs
@@ -11,63 +11,16 @@ namespace BlazorFiles.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ExcelEPPlusController : ControllerBase
+    public class ExcelEPPlusUsedRangeController : ControllerBase
     {
         //
         enum RowOrCol { Row, Column };
 
         private readonly IHostEnvironment _environment;
 
-        public ExcelEPPlusController(IHostEnvironment environment)
+        public ExcelEPPlusUsedRangeController(IHostEnvironment environment)
         {
             _environment = environment;
-        }
-
-        private static void ConventionalRemoveEmptyRowsCols(ExcelWorksheet worksheet)
-        {
-            ExcelAddressBase usedRange = worksheet.Dimension;
-            int totalRows = usedRange.Rows;
-            int totalCols = usedRange.Columns;
-
-            RemoveEmpty(usedRange, RowOrCol.Row);
-            RemoveEmpty(usedRange, RowOrCol.Column);
-        }
-
-        private static void RemoveEmpty(ExcelAddressBase usedRange, RowOrCol rowOrCol)
-        {
-            int count;
-            ExcelAddressBase curRange;
-            if (rowOrCol == RowOrCol.Column)
-                count = usedRange.Columns;
-            else
-                count = usedRange.Rows;
-
-            for (int i = count; i > 0; i--)
-            {
-                bool isEmpty = true;
-                if (rowOrCol == RowOrCol.Column)
-                    curRange = usedRange;
-                else
-                    curRange = usedRange.Rows;
-
-                foreach (ExcelRange cell in curRange.Cells)
-                {
-                    if (cell.Value != null)
-                    {
-                        isEmpty = false;
-                        break; // we can exit this loop since the range is not empty
-                    }
-                    else
-                    {
-                        // Cell value is null contiue checking
-                    }
-                } // end loop thru each cell in this range (row or column)
-
-                if (isEmpty)
-                {
-                    curRange.Delete();
-                }
-            }
         }
 
         [HttpPost]
