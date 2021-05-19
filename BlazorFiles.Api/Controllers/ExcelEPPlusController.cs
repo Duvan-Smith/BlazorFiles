a666d2a440be5f73490e13e03a1f955eaf76879e
Duvan Smith Montenegro Roque <dsmith.mr@gmail.com>
2021-05-19 08:08:10 -0500

    EPPlus funcional

--- BlazorFiles.Api/Controllers/ExcelEPPlusController.cs
+++ BlazorFiles.Api/Controllers/ExcelEPPlusController.cs
@@ -44,46 +44,19 @@ namespace BlazorFiles.Api.Controllers
                 _ = fileStream.FlushAsync();
             }
 
-            var package = new ExcelPackage(new FileInfo(@filePath));
-            ExcelWorksheet sheet = package.Workbook.Worksheets[1];
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var package = new ExcelPackage(new FileInfo(filePath));
 
-            var table = sheet.Tables.First();
-
-            //FileInfo existingFile = new FileInfo(filePath);
-            //using (ExcelPackage package = new ExcelPackage(existingFile))
-            //{
-            //    //get the first worksheet in the workbook
-            //    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
-            //    int colCount = worksheet.Dimension.End.Column;  //get Column Count
-            //    int rowCount = worksheet.Dimension.End.Row;     //get row count
-            //    for (int row = 1; row <= rowCount; row++)
-            //    {
-            //        for (int col = 1; col <= colCount; col++)
-            //        {
-            //            Console.WriteLine(" Row:" + row + " column:" + col + " Value:" + worksheet.Cells[row, col].Value?.ToString().Trim());
-            //        }
-            //    }
-            //}
-
-            //FileInfo fileInfo = new FileInfo(filePath);
-
-            //ExcelPackage package = new ExcelPackage(fileInfo);
-
-            //ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
-
-            //int rows = worksheet.Dimension.Rows; // 20
-            //int columns = worksheet.Dimension.Columns; // 7
-
-            //// loop through the worksheet rows and columns
-            //for (int i = 1; i <= rows; i++)
-            //{
-            //    for (int j = 1; j <= columns; j++)
-            //    {
-
-            //        string content = worksheet.Cells[i, j].Value.ToString();
-            //        /* Do something ...*/
-            //    }
-            //}
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+            int colCount = worksheet.Dimension.End.Column;  //get Column Count
+            int rowCount = worksheet.Dimension.End.Row;     //get row count
+            for (int row = 1; row <= rowCount; row++)
+            {
+                for (int col = 1; col <= colCount; col++)
+                {
+                    Console.WriteLine(" Row:" + row + " column:" + col + " Value:" + worksheet.Cells[row, col].Value?.ToString().Trim());
+                }
+            }
 
             return Ok($"Excel/{newFileName}");
         }
