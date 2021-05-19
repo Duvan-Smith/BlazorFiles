3129d0106bd7cf5d6f0115691156e16d820095bc
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-05-19 14:41:21 -0500

    new idea for implement in Epplus, don't working

--- BlazorFiles.Api/Controllers/ExcelEpplusController.cs
+++ BlazorFiles.Api/Controllers/ExcelEpplusController.cs
@@ -13,11 +13,61 @@ namespace BlazorFiles.Api.Controllers
     [ApiController]
     public class ExcelEPPlusController : ControllerBase
     {
+        //
+        enum RowOrCol { Row, Column };
+
         private readonly IHostEnvironment _environment;
 
         public ExcelEPPlusController(IHostEnvironment environment)
         {
             _environment = environment;
+        }
+
+        private static void ConventionalRemoveEmptyRowsCols(ExcelWorksheet worksheet)
+        {
+            ExcelAddressBase usedRange = worksheet.Dimension;
+            int totalRows = usedRange.Rows;
+            int totalCols = usedRange.Columns;
+
+            RemoveEmpty(usedRange, RowOrCol.Row);
+            RemoveEmpty(usedRange, RowOrCol.Column);
+        }
+
+        private static void RemoveEmpty(ExcelAddressBase usedRange, RowOrCol rowOrCol)
+        {
+            int count;
+            ExcelAddressBase curRange;
+            if (rowOrCol == RowOrCol.Column)
+                count = usedRange.Columns;
+            else
+                count = usedRange.Rows;
+
+            for (int i = count; i > 0; i--)
+            {
+                bool isEmpty = true;
+                if (rowOrCol == RowOrCol.Column)
+                    curRange = usedRange;
+                else
+                    curRange = usedRange.Rows;
+
+                foreach (ExcelRange cell in curRange.Cells)
+                {
+                    if (cell.Value != null)
+                    {
+                        isEmpty = false;
+                        break; // we can exit this loop since the range is not empty
+                    }
+                    else
+                    {
+                        // Cell value is null contiue checking
+                    }
+                } // end loop thru each cell in this range (row or column)
+
+                if (isEmpty)
+                {
+                    curRange.Delete();
+                }
+            }
         }
 
         [HttpPost]
@@ -51,13 +101,18 @@ namespace BlazorFiles.Api.Controllers
             int colCount = worksheet.Dimension.End.Column;  //get Column Count
             int rowCount = worksheet.Dimension.End.Row;     //get row count
 
-            for (int row = 1; row <= rowCount; row++)
-            {
-                for (int col = 1; col <= colCount; col++)
-                {
-                    Console.WriteLine(" Row:" + row + " column:" + col + " Value:" + worksheet.Cells[row, col].Value?.ToString().Trim());
-                }
-            }
+
+
+            //for (int row = 1; row <= rowCount; row++)
+            //{
+            //    for (int col = 1; col <= colCount; col++)
+            //    {
+            //        Console.WriteLine(" Row:" + row + " column:" + col + " Value:" + worksheet.Cells[row, col].Value?.ToString().Trim());
+            //    }
+            //}
+
+            //    enum RowOrCol { Row, Column };
+
 
             return Ok($"Excel/{newFileName}");
         }
